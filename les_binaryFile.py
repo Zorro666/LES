9a2392abfac4f13f68ad8e75c57167f7bb873744
Jake <jake@evansturner.co.uk>
2011-08-25 07:15:01 +0100

    support for choosing big or little endian

--- les_binaryFile.py
+++ les_binaryFile.py
@@ -1,26 +1,40 @@
-
 import struct
 
 class LES_BinaryFile():	
 	def __init__(self, fname):
 		self.fname = fname
 		self.fh= open(fname, mode="wb")
+		self.endianFormat = ""
+		self.setBigEndian()
+
+	def setFormats(self):
+		self.intFormat = self.endianFormat + "i"
+		self.uintFormat = self.endianFormat + "I"
+
+	def setLittleEndian(self):
+		self.endianFormat = "<"
+		self.setFormats()
+
+	def setBigEndian(self):
+		self.endianFormat = ">"
+		self.setFormats()
 
 	def writeInt(self, value):
-		temp = struct.pack(">i", value)
+		temp = struct.pack(self.intFormat, value)
 		self.fh.write(temp)
 
 	def writeUint(self, value):
-		temp = struct.pack(">I", value)
+		temp = struct.pack(self.uintFormat, value)
 		self.fh.write(temp)
 
 	def writeString(self, value):
 		stringLen = len(value)
-		strFmt=">"+str(stringLen)+"s"
+		strFmt = self.endianFormat + str(stringLen) + "s"
 		temp = struct.pack(strFmt, value)
 		self.fh.write(temp)
 		# null terminate the string
-		temp = struct.pack(">1c", chr(0))
+		strFmt = self.endianFormat + "1c"
+		temp = struct.pack(strFmt, chr(0))
 		self.fh.write(temp)
 
 	def close(self):
