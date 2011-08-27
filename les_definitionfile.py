85e9d8765a9a8a612d807c502c93880615ebc076
Jake <jake@evansturner.co.uk>
2011-08-27 07:32:16 +0100

    made it a class and add arbitrary chunk datas and it will output them for you

--- les_definitionfile.py
+++ les_definitionfile.py
@@ -11,6 +11,32 @@ import les_typedata
 #	FUNCTION_DATA_CHUNK
 #}
 
+class LES_DefinitionFile():
+	def __init__(self):
+		self.fname = ""
+		self.chunkDatas = []
+		self.chunkNames = []
+		self.numChunks = len(self.chunkDatas)
+
+	def addChunk(self, chunkName, chunkData):
+		self.chunkDatas.append(chunkData)
+		self.chunkNames.append(chunkName)
+		self.numChunks = len(self.chunkDatas)
+
+	def writeFile(self, fname):
+		self.fname = fname
+		chunkFile = les_chunkfile.LES_ChunkFile(self.fname, "LESD", self.numChunks)
+
+		for i in range(self.numChunks):
+			chunkData = self.chunkDatas[i]
+			chunkName = self.chunkNames[i]
+
+			binFile = chunkFile.startChunk(chunkName)
+			chunkData.writeFile(binFile)
+			chunkFile.endChunk()
+
+		chunkFile.close()
+
 def runTest():
 	stringTable = les_stringtable.LES_StringTable()
 	stringTable.addString("jake")
@@ -22,17 +48,10 @@ def runTest():
 	index = typeData.addType("int*", 4, les_typedata.LES_TYPE_INPUT_OUTPUT|les_typedata.LES_TYPE_POINTER|les_typedata.LES_TYPE_ALIAS, "int")
 	index = typeData.addType("int[3]", 4*3, les_typedata.LES_TYPE_INPUT_OUTPUT|les_typedata.LES_TYPE_ARRAY|les_typedata.LES_TYPE_ALIAS, "int*", 3)
 
-	chunkFile = les_chunkfile.LES_ChunkFile("chunkTest.bin", "LESD", 2)
-
-	binFile = chunkFile.startChunk("StringTable")
-	stringTable.writeFile(binFile)
-	chunkFile.endChunk()
-
-	binFile = chunkFile.startChunk("TypeData")
-	typeData.writeFile(binFile)
-	chunkFile.endChunk()
-
-	chunkFile.close()
+	this = LES_DefinitionFile()
+	this.addChunk("StringTable", stringTable)
+	this.addChunk("TypeData", typeData)
+	this.writeFile("defTest.bin")
 
 if __name__ == '__main__':
 	runTest()
