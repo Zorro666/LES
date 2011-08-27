9115d22346b02736660ad1421ec1a8581cfc8cac
Jake <jake@evansturner.co.uk>
2011-08-27 07:20:15 +0100

    added basic typeData chunk and output

--- les_definitionfile.py
+++ les_definitionfile.py
@@ -2,6 +2,7 @@
 
 import les_chunkfile
 import les_stringtable
+import les_typedata
 
 #{
 #	STRING TABLE_CHUNK
@@ -16,10 +17,19 @@ def runTest():
 	stringTable.addString("rowan")
 	stringTable.addString("Jake")
 
-	chunkFile = les_chunkfile.LES_ChunkFile("chunkTest.bin", "LESD", 1)
+	typeData = les_typedata.LES_TypeData(stringTable)
+	index = typeData.addType("int", 4, les_typedata.LES_TYPE_INPUT|les_typedata.LES_TYPE_POD)
+	index = typeData.addType("int*", 4, les_typedata.LES_TYPE_INPUT_OUTPUT|les_typedata.LES_TYPE_POINTER|les_typedata.LES_TYPE_ALIAS, "int")
+	index = typeData.addType("int[3]", 4*3, les_typedata.LES_TYPE_INPUT_OUTPUT|les_typedata.LES_TYPE_ARRAY|les_typedata.LES_TYPE_ALIAS, "int*", 3)
+
+	chunkFile = les_chunkfile.LES_ChunkFile("chunkTest.bin", "LESD", 2)
 
 	binFile = chunkFile.startChunk("StringTable")
 	stringTable.writeFile(binFile)
+	chunkFile.endChunk()
+
+	binFile = chunkFile.startChunk("TypeData")
+	typeData.writeFile(binFile)
 	chunkFile.endChunk()
 
 	chunkFile.close()
