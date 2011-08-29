e92ebd8df6aef94f778826aba26e736688b96867
Jake <jake@evansturner.co.uk>
2011-08-29 22:04:50 +0100

    made some members private added getStringByHash() function

--- les_stringtable.py
+++ les_stringtable.py
@@ -24,53 +24,61 @@ class LES_StringEntry():
 
 class LES_StringTable():
 	def __init__(self):
-		self.strings = []
-		self.stringEntries = []
+		self.__m_strings__ = []
+		self.__m_stringEntries__ = []
 
 	def addString(self, string):
 		index = self.getStringID(string)
 		if index >= 0:
 		 return index
 
-		index = len(self.strings)
-		self.strings.append(string)
+		index = len(self.__m_strings__)
+		self.__m_strings__.append(string)
 
 		hashValue = les_hash.GenerateHashCaseSensitive(string)
 
 		offset = 0
 		if index > 0:
-			offset = self.stringEntries[index-1].offset
+			offset = self.__m_stringEntries__[index-1].offset
 
 			# Add on the length of the previous string
-			prevStringLen = len(self.strings[index-1])
+			prevStringLen = len(self.__m_strings__[index-1])
 
 			# include the null terminator
 			prevStringLen += 1
 			offset += prevStringLen
 
 		stringEntry = LES_StringEntry(hashValue, offset)
-		self.stringEntries.append(stringEntry)
+		self.__m_stringEntries__.append(stringEntry)
 
 		return index
 
 	def getString(self, index):
-		if index < len(self.strings):
-			return self.strings[index]
+		if index < len(self.__m_strings__):
+			return self.__m_strings__[index]
 		return -1
 
 	def getHash(self, index):
 		if index < len(self.hashes):
-			return self.stringEntries[index].hashValue
+			return self.__m_stringEntries__[index].hashValue
 		return -1
 
 	def getStringID(self, string):
 		index = -1
 		try:
-			index = self.strings.index(string)
+			index = self.__m_strings__.index(string)
 		except ValueError:
 			index = -1
 
 		return index
+
+	def getStringByHash(self, hashValue):
+		numStrings = len(self.__m_strings__)
+		for index in range(numStrings):
+			stringEntry = self.__m_stringEntries__[index]
+			if stringEntry.hashValue == hashValue:
+				return self.__m_strings__[index]
+		return None
 
 	def writeFile(self, binFile):
 		# LES_StringTable
@@ -81,11 +89,11 @@ class LES_StringTable():
 		# };
 
 		#	int m_numStrings; - 4 bytes
-		numStrings = len(self.strings)
+		numStrings = len(self.__m_strings__)
 		binFile.writeInt(numStrings)	
 
 		#	LES_StringEntry stringEntry[numStrings]; 				- 8 bytes * numStrings
-		for stringEntry in self.stringEntries:
+		for stringEntry in self.__m_stringEntries__:
 			#	struct LES_StringEntry
 			#	{
 			#		unsigned int m_hash;												- 4 bytes
@@ -99,7 +107,7 @@ class LES_StringTable():
 			binFile.writeInt(stringEntry.offset)	
 
 		#	char stringData[];															- total string table size in bytes
-		for string in self.strings:
+		for string in self.__m_strings__:
 			binFile.writeCstring(string)	
 
 def runTest():
