ff288206c212e12aa000c4f0b925592241e115d0
Jake <jake@evansturner.co.uk>
2011-08-30 20:42:18 +0100

    added fixed sized int types added convert from little endian functions

--- les_base.h
+++ les_base.h
@@ -3,9 +3,97 @@
 
 typedef unsigned int LES_uint;
 
+typedef signed char LES_int8;
+typedef unsigned char LES_uint8;
+
+typedef signed short LES_int16;
+typedef unsigned short LES_uint16;
+
+typedef signed int LES_int32;
+typedef unsigned int LES_uint32;
+
 #define LES_NULL (0)
 
 #define LES_RETURN_OK (1)
 #define LES_RETURN_ERROR (-1)
+
+#define BIG_ENDIAN_MACHINE (1)
+
+// Swap BigEndian -> LittleEndian
+LES_int16 fromLittleEndian16(const LES_int16 input);
+LES_uint16 fromLittleEndian16(const LES_uint16 input);
+
+LES_int32 fromLittleEndian32(const LES_int32 input);
+LES_uint32 fromLittleEndian32(const LES_uint32 input);
+
+#if BIG_ENDIAN_MACHINE
+inline LES_int16 fromLittleEndian16(const LES_int16 input)
+{
+	// Bytes: input = A:B, output = B:A
+	const LES_uint32 value = input;
+	const LES_uint32 A = (((value & 0xFF00) >> 8) & 0xFF);
+	const LES_uint32 B = (((value & 0x00FF) >> 0) & 0xFF);
+	const LES_uint32 swapValue = (A << 0) | (B << 8);
+	const LES_uint16 output = (LES_uint16)swapValue;
+	return output;
+}
+
+inline LES_uint16 fromLittleEndian16(const LES_uint16 input)
+{
+	// Bytes: input = A:B, output = B:A
+	const LES_uint32 value = input;
+	const LES_uint32 A = (((value & 0xFF00) >> 8) & 0xFF);
+	const LES_uint32 B = (((value & 0x00FF) >> 0) & 0xFF);
+	const LES_uint32 swapValue = (A << 0) | (B << 8);
+	const LES_uint16 output = (LES_uint16)swapValue;
+	return output;
+}
+
+inline LES_int32 fromLittleEndian32(const LES_int32 input)
+{
+	// Bytes: input = A:B:C:D, output = D:C:B:A
+	const LES_uint32 value = input;
+	const LES_uint32 A = (((value & 0xFF000000) >> 24) & 0xFF);
+	const LES_uint32 B = (((value & 0x00FF0000) >> 16) & 0xFF);
+	const LES_uint32 C = (((value & 0x0000FF00) >> 8) & 0xFF);
+	const LES_uint32 D = (((value & 0x000000FF) >> 0) & 0xFF);
+	const LES_uint32 swapValue = (A << 0) | (B << 8) | (C << 16) | (D << 24);
+	const LES_uint32 output = swapValue;
+	return output;
+}
+
+inline LES_uint32 fromLittleEndian32(const LES_uint32 input)
+{
+	// Bytes: input = A:B:C:D, output = D:C:B:A
+	const LES_uint32 value = input;
+	const LES_uint32 A = (((value & 0xFF000000) >> 24) & 0xFF);
+	const LES_uint32 B = (((value & 0x00FF0000) >> 16) & 0xFF);
+	const LES_uint32 C = (((value & 0x0000FF00) >> 8) & 0xFF);
+	const LES_uint32 D = (((value & 0x000000FF) >> 0) & 0xFF);
+	const LES_uint32 swapValue = (A << 0) | (B << 8) | (C << 16) | (D << 24);
+	const LES_uint32 output = swapValue;
+	return output;
+}
+#else // #if BIG_ENDIAN_MACHINE
+inline LES_int16 fromLittleEndian16(const LES_int16 input)
+{
+	return input;
+}
+
+inline LES_uint16 fromLittleEndian16(const LES_uint16 input)
+{
+	return input;
+}
+
+inline LES_int32 fromLittleEndian32(const LES_int32 input)
+{
+	return input;
+}
+
+inline LES_uint32 fromLittleEndian32(const LES_uint32 input)
+{
+	return input;
+}
+#endif // #if BIG_ENDIAN_MACHINE
 
 #endif // #ifndef LES_BASE_HH
