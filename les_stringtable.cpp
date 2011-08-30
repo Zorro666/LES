eb95de1681805ffcdc321cb65a7821d2dd15ff20
Jake <jake@evansturner.co.uk>
2011-08-30 21:19:13 +0100

    settle the data on load * convert from little endian * settle string table offsets into string pointers

--- les_stringtable.cpp
+++ les_stringtable.cpp
@@ -7,15 +7,31 @@ int LES_StringTable::Settle(void)
 	{
 		return LES_RETURN_ERROR;
 	}
+
+	// Convert from little endian first
+	m_numStrings = fromLittleEndian32(m_numStrings);
+	m_settled = fromLittleEndian32(m_settled);
+	LES_LOG("m_numStrings:%d", m_numStrings);
+	LES_LOG("m_settled:%d", m_settled);
+
 	const int numStrings = m_numStrings;
-	const char* pBase = (const char*)&m_numStrings;
+	for (int i = 0; i < numStrings; i++)
+	{
+		m_stringTableEntries[i].m_hash = fromLittleEndian32(m_stringTableEntries[i].m_hash);
+		m_stringTableEntries[i].m_offset = fromLittleEndian32(m_stringTableEntries[i].m_offset);
+	}
+
+	const char* pStringTableStart = (const char*)&m_numStrings;
+	pStringTableStart += 8;
+	pStringTableStart += (numStrings * 8);
 	for (int i = 0; i < numStrings; i++)
 	{
 		LES_StringTableEntry* const pStringTableEntry = &m_stringTableEntries[i];
 		const int offset = pStringTableEntry->m_offset;
-		const char* const pString = pBase + offset;
+		const char* const pString = pStringTableStart + offset;
 		LES_StringEntry* const pStringEntry = (LES_StringEntry* const)(&m_stringTableEntries[i]);
 		pStringEntry->m_str = pString;
+		LES_LOG("String[%d] hash:0x%X offset:%d string:'%s'", i, pStringEntry->m_hash, offset, pStringEntry->m_str);
 	}
 	m_settled = 1;
 
