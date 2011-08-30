c40a1829245045e9367f3b38f7db96ec568e8f8a
Jake <jake@evansturner.co.uk>
2011-08-30 20:42:49 +0100

    use fixed size int types use fromLittleEndian conversion function

--- les_chunkfile.h
+++ les_chunkfile.h
@@ -17,8 +17,8 @@ private:
 	~LES_ChunkFile();
 
 	char m_ID[4];
-	LES_uint m_numChunks;
-	int m_chunkOffsets[1];	// m_chunkOffsets[m_numChunks]
+	LES_uint32 m_numChunks;
+	LES_int32 m_chunkOffsets[1];	// m_chunkOffsets[m_numChunks]
 	// chunkData[0]
 	// chunkData[1]
 	// ....
@@ -32,12 +32,12 @@ inline const char* LES_ChunkFile::GetID(void) const
 
 inline int LES_ChunkFile::GetNumChunks(void) const
 {
-	return m_numChunks;
+	return fromLittleEndian32(m_numChunks);
 }
 
 inline const void* LES_ChunkFile::GetChunkData(const int chunk) const
 {
-	const int chunkOffset = m_chunkOffsets[chunk];
+	const int chunkOffset = fromLittleEndian32(m_chunkOffsets[chunk]);
 	const char* basePtr = m_ID;
 	const char* chunkPtr = basePtr + chunkOffset;
 	return chunkPtr;
