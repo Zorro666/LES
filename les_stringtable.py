894b22f8e864427f69a0c2f0d2c49bb5f4a4e4ca
Jake <jake@evansturner.co.uk>
2011-08-30 18:34:26 +0100

    updated comments & variable names to show it is outputting LES_StringTableEntry

--- les_stringtable.py
+++ les_stringtable.py
@@ -4,28 +4,28 @@ import les_hash
 import les_binaryfile
 import les_logger
 
-#	struct LES_StringEntry
+#	struct LES_StringTableEntry
 #	{
 #		unsigned int m_hash;													- 4 bytes
-#		const char* m_str;														- 4 bytes : saved as offset into stringTable, ptr settled on load
+#		const char* m_str;														- 4 bytes : saved as (int) offset into stringTable, ptr settled on load
 #	};
 #
-class LES_StringEntry():
+class LES_StringTableEntry():
 	def __init__(self, hashValue, offset):
 		self.hashValue = hashValue
 		self.offset = offset
 
 # LES_StringTable
 # {
-# 	int m_numStrings; 														- 4 bytes
-#		LES_StringEntry m_stringEntries[numStrings]; 	- 8 bytes * m_numStrings
-#		char stringData[];														- total string table size in bytes
+# 	int m_numStrings; 																			- 4 bytes
+#		LES_StringTableEntry m_stringTableEntries[numStrings]; 	- 8 bytes * m_numStrings
+#		char stringData[];																			- total string table size in bytes
 # };
 
 class LES_StringTable():
 	def __init__(self):
 		self.__m_strings__ = []
-		self.__m_stringEntries__ = []
+		self.__m_stringTableEntries__ = []
 
 	def addString(self, string):
 		index = self.getStringID(string)
@@ -39,7 +39,7 @@ class LES_StringTable():
 
 		offset = 0
 		if index > 0:
-			offset = self.__m_stringEntries__[index-1].offset
+			offset = self.__m_stringTableEntries__[index-1].offset
 
 			# Add on the length of the previous string
 			prevStringLen = len(self.__m_strings__[index-1])
@@ -48,8 +48,8 @@ class LES_StringTable():
 			prevStringLen += 1
 			offset += prevStringLen
 
-		stringEntry = LES_StringEntry(hashValue, offset)
-		self.__m_stringEntries__.append(stringEntry)
+		stringTableEntry = LES_StringTableEntry(hashValue, offset)
+		self.__m_stringTableEntries__.append(stringTableEntry)
 
 		return index
 
@@ -60,7 +60,7 @@ class LES_StringTable():
 
 	def getHash(self, index):
 		if index < len(self.hashes):
-			return self.__m_stringEntries__[index].hashValue
+			return self.__m_stringTableEntries__[index].hashValue
 		return -1
 
 	def getStringID(self, string):
@@ -75,36 +75,36 @@ class LES_StringTable():
 	def getStringByHash(self, hashValue):
 		numStrings = len(self.__m_strings__)
 		for index in range(numStrings):
-			stringEntry = self.__m_stringEntries__[index]
-			if stringEntry.hashValue == hashValue:
+			stringTableEntry = self.__m_stringTableEntries__[index]
+			if stringTableEntry.hashValue == hashValue:
 				return self.__m_strings__[index]
 		return None
 
 	def writeFile(self, binFile):
 		# LES_StringTable
 		# {
-		# 	int m_numStrings; 														- 4 bytes
-		#		LES_StringEntry m_stringEntries[numStrings]; 	- 8 bytes * m_numStrings
-		#		char stringData[];														- total string table size in bytes
+		# 	int m_numStrings; 																			- 4 bytes
+		#		LES_StringTableEntry m_stringTableEntries[numStrings]; 	- 8 bytes * m_numStrings
+		#		char stringData[];																			- total string table size in bytes
 		# };
 
 		#	int m_numStrings; - 4 bytes
 		numStrings = len(self.__m_strings__)
 		binFile.writeInt(numStrings)	
 
-		#	LES_StringEntry stringEntry[numStrings]; 				- 8 bytes * numStrings
-		for stringEntry in self.__m_stringEntries__:
-			#	struct LES_StringEntry
+		#	LES_StringTableEntry m_stringTableEntries[numStrings]; 		- 8 bytes * m_numStrings
+		for stringTableEntry in self.__m_stringTableEntries__:
+			#	struct LES_StringTableEntry
 			#	{
 			#		unsigned int m_hash;												- 4 bytes
 			#		const char* m_str;													- 4 bytes : saved as offset into stringTable, ptr settled on load
 			#	};
 
 			#		unsigned int m_hash;												- 4 bytes
-			binFile.writeUint(stringEntry.hashValue)	
+			binFile.writeUint(stringTableEntry.hashValue)	
 
 			#		const char* m_str;													- 4 bytes : saved as offset into stringTable, ptr settled on load
-			binFile.writeInt(stringEntry.offset)	
+			binFile.writeInt(stringTableEntry.offset)	
 
 		#	char stringData[];															- total string table size in bytes
 		for string in self.__m_strings__:
