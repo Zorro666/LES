505fd021a1a20245c4edbc15deda9e17af6994c9
Jake <jake@evansturner.co.uk>
2011-09-03 13:41:18 +0100

    specific write sized types (8, 16, 32)

--- les_binaryfile.py
+++ les_binaryfile.py
@@ -11,8 +11,12 @@ class LES_BinaryFile():
 		self.setBigEndian()
 
 	def setFormats(self):
-		self.intFormat = self.endianFormat + "i"
-		self.uintFormat = self.endianFormat + "I"
+		self.int8Format = self.endianFormat + "b"
+		self.uint8Format = self.endianFormat + "B"
+		self.int16Format = self.endianFormat + "h"
+		self.uint16Format = self.endianFormat + "H"
+		self.int32Format = self.endianFormat + "i"
+		self.uint32Format = self.endianFormat + "I"
 
 	def setLittleEndian(self):
 		self.endianFormat = "<"
@@ -22,12 +26,28 @@ class LES_BinaryFile():
 		self.endianFormat = ">"
 		self.setFormats()
 
-	def writeInt(self, value):
-		temp = struct.pack(self.intFormat, value)
+	def writeInt8(self, value):
+		temp = struct.pack(self.int8Format, value)
 		self.fh.write(temp)
 
-	def writeUint(self, value):
-		temp = struct.pack(self.uintFormat, value)
+	def writeUint8(self, value):
+		temp = struct.pack(self.uint8Format, value)
+		self.fh.write(temp)
+
+	def writeInt16(self, value):
+		temp = struct.pack(self.int16Format, value)
+		self.fh.write(temp)
+
+	def writeUint16(self, value):
+		temp = struct.pack(self.uint16Format, value)
+		self.fh.write(temp)
+
+	def writeInt32(self, value):
+		temp = struct.pack(self.int32Format, value)
+		self.fh.write(temp)
+
+	def writeUint32(self, value):
+		temp = struct.pack(self.uint32Format, value)
 		self.fh.write(temp)
 
 	def writeString(self, value):
@@ -56,17 +76,17 @@ def runTest():
 	les_logger.Init()
 	this = LES_BinaryFile("testLittle.bin")
 	this.setLittleEndian()
-	this.writeInt(15)
-	this.writeInt(0x987654)
-	this.writeUint(0xDEADBEAF)
+	this.writeInt32(15)
+	this.writeInt32(0x987654)
+	this.writeUint32(0xDEADBEAF)
 	this.writeString("testLit")
 	this.close()
 
 	this = LES_BinaryFile("testBig.bin")
 	this.setBigEndian()
-	this.writeInt(15)
-	this.writeInt(0x987654)
-	this.writeUint(0xDEADBEAF)
+	this.writeInt32(15)
+	this.writeInt32(0x987654)
+	this.writeUint32(0xDEADBEAF)
 	this.writeString("testBig")
 	this.close()
 
