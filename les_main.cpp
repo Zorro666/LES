248ae115d6db84e864c39f7c72bd2edfd2677be4
Jake <jake@evansturner.co.uk>
2011-09-08 07:37:41 +0100

    prototype network client connect send & receive messages running on its own thread needs a lot of tidying and extending and moving to its own file

--- les_main.cpp
+++ les_main.cpp
@@ -26,6 +26,108 @@ void JAKE_Test()
 #endif // #if 0
 }
 
+struct NetworkThreadStartStruct
+{
+	int m_hsock;
+};
+
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
+void* clientNetworkThread(void* args);
+
+void JAKE_SocketTest()
+{
+  const int hsock = socket(AF_INET, SOCK_STREAM, 0);
+  if (hsock == -1)
+	{
+      printf("Error initializing socket %d\n",errno);
+      return;
+  }
+    
+	int option = 1;
+        
+  if ((setsockopt(hsock, SOL_SOCKET, SO_REUSEADDR, (char*)&option, sizeof(int)) == -1) ||
+      (setsockopt(hsock, SOL_SOCKET, SO_KEEPALIVE, (char*)&option, sizeof(int)) == -1))
+	{
+    printf("Error setting options %d\n",errno);
+		close(hsock);
+    return;
+  }
+
+  short host_port = 3141;
+  const char* const host_name = "127.0.0.1";
+  struct sockaddr_in my_addr;
+	my_addr.sin_family = AF_INET ;
+ 	my_addr.sin_port = htons(host_port);
+    
+ 	memset(&(my_addr.sin_zero), 0, 8);
+	my_addr.sin_addr.s_addr = inet_addr(host_name);
+
+ 	if (connect(hsock, (struct sockaddr*)&my_addr, sizeof(my_addr)) == -1)
+	{
+		int err = errno;
+		if (err != EINPROGRESS)
+		{
+			fprintf(stderr, "Error connecting socket errno:0x%X\n", errno);
+  		close(hsock);
+      return;
+		}
+ 	}
+	pthread_t networkThread;
+	static NetworkThreadStartStruct networkThreadStartStruct;
+	networkThreadStartStruct.m_hsock = hsock;
+	int ret = pthread_create(&networkThread, NULL, clientNetworkThread, &networkThreadStartStruct);
+	LES_LOG("ret = %d", ret);
+}
+
+void* clientNetworkThread(void* args)
+{
+	const NetworkThreadStartStruct* const networkThreadStartStruct = (const NetworkThreadStartStruct* const)args;
+	LES_LOG("clientNetworkThread Started");
+
+	const int hsock = networkThreadStartStruct->m_hsock;
+
+	//Now lets do the client related stuff
+	while (1)
+	{
+		const int bufferLen = 1024;
+  	char buffer[1024];
+		memset(buffer, '\0', bufferLen);
+
+	  printf("Enter some text to send to the server (press enter)\n");
+		fgets(buffer, 1024, stdin);
+		buffer[strlen(buffer)-1]='\0';
+			
+		int bytecount;
+		bytecount = send(hsock, buffer, strlen(buffer),0);
+		if (bytecount == -1)
+		{
+			fprintf(stderr, "Error sending data %d\n", errno);
+			close(hsock);
+			return LES_NULL;
+		}
+		printf("Sent bytes %d\n", bytecount);
+
+		buffer[0] = '\0';
+		bytecount = recv(hsock, buffer, bufferLen, 0);
+		if (bytecount == -1)
+		{
+			fprintf(stderr, "Error receiving data %d\n", errno);
+			close(hsock);
+			return LES_NULL;
+		}
+		printf("Received bytes %d\nReceived string \"%s\"\n", bytecount, buffer);
+	}
+
+  close(hsock);
+	LES_LOG("clientNetworkThread Ended");
+	return LES_NULL;
+}
+
 int JAKE_LoadDefinitionFile(const char* const fname)
 {
 	FILE* fh = fopen(fname, "rb");
@@ -73,6 +175,19 @@ int main(const int argc, const char* const argv[])
 	LES_Logger::Init();
 	LES_Logger::SetConsoleOutput(LES_Logger::CHANNEL_LOG, verbose);
 	LES_Init();
+	JAKE_SocketTest();
+	float lastTime = -10000.0f;
+	while (1)
+	{
+		const float logDelta = 1.0f;
+		const float elapsedTime = LES_GetElapsedTimeInSeconds();
+		if ((elapsedTime - lastTime) > logDelta)
+		{
+			LES_LOG("Time %f", elapsedTime);
+			lastTime = elapsedTime;
+		}
+		LES_Sleep(2.5f);
+	}
 
 	if (JAKE_LoadDefinitionFile("defTest.bin") != LES_RETURN_OK)
 	{
