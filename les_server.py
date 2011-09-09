0ea3a7267a5c694ce8f2210b1c60c173b45819ad
Jake <jake@evansturner.co.uk>
2011-09-09 06:24:07 +0100

    WIP - decode message struct Need to send back a message struct Need to decodee message in big endian format (need to send it in big endian format)

--- les_server.py
+++ les_server.py
@@ -3,15 +3,28 @@
 import time
 import threading
 import SocketServer
+import struct
+
+packedInt16 = struct.Struct("<h")
+packedUint16 = struct.Struct("<H")
+packedInt32 = struct.Struct("<i")
+packedUint32 = struct.Struct("<I")
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
+		curThread = threading.currentThread()
+		threadName = curThread.getName()
 		while (1):
- 			data = self.request.recv(1024)
-			curThread = threading.currentThread()
-			threadName = curThread.getName()
-			print("%s:'%s'" % (threadName, data))
- 			response = "%s: %s" % (threadName, data)
+ 			message = self.request.recv(1024)
+
+			typeValue = packedInt16.unpack(message[0:2])[0]
+			idValue = packedInt16.unpack(message[2:4])[0]
+			payloadSize = packedInt32.unpack(message[4:8])[0]
+
+			print("type:0x%X id:%d payloadSize:%d" % (typeValue, idValue, payloadSize))
+			messageData = message[8:]
+			print("messageData:%s" % (messageData))
+ 			response = "%s: %s" % (threadName, messageData)
 			self.request.send(response)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
