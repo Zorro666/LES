12ef12adeec2b629db0eff0f68b386156af612bc
Jake <jake@evansturner.co.uk>
2011-09-16 19:40:25 +0100

    import les_definitionfile send definition file data back in response to GETDEFINITIONFILE message

--- les_server.py
+++ les_server.py
@@ -6,6 +6,8 @@ import SocketServer
 import struct
 
 import les_hash
+import les_definitionfile
+import les_logger
 
 packedUint16 = struct.Struct(">H")
 packedUint32 = struct.Struct(">I")
@@ -36,7 +38,9 @@ class LES_NetworkMessage():
 
 def LES_CreateNetworkMessage(typeValue, idValue, payload):
 	# Needs to do memory padding like the client does on its sending
-	payloadSize = len(payload)
+	payloadSize = 0
+	if payload != None:
+		payloadSize = len(payload)
 
 	packetData = ""
 	packetData += packedUint16.pack(typeValue)
@@ -47,34 +51,49 @@ def LES_CreateNetworkMessage(typeValue, idValue, payload):
 
 	return packetData
 
-LES_NETMESSAGE_SEND_ID_CONNECT_RESPONSE=0x2
-LES_NETMESSAGE_SEND_ID_TEST=0x33
-
 LES_NETMESSAGE_RECV_ID_CONNECT=0x1
-LES_NETMESSAGE_RECV_ID_TEST=0x66
+LES_NETMESSAGE_RECV_ID_GETDEFINITIONFILE=0x3
+LES_NETMESSAGE_RECV_ID_TEST=0xF1
+
+LES_NETMESSAGE_SEND_ID_CONNECT_RESPONSE=0x2
+LES_NETMESSAGE_SEND_ID_GETDEFINITIONFILE_RESPONSE=0x4
+LES_NETMESSAGE_SEND_ID_TEST_RESPONSE=0xF2
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 	def LES_HandleTestMessage(self, msgType, msgId, msgPayloadSize, msgPayload):
-		print("Test: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
-		print("Test: payload:%s" % (msgPayload))
+		les_logger.Log("Test: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
+		les_logger.Log("Test: payload:%s" % (msgPayload))
 
 		payload = "%s:%s" % (self.m_threadName, msgPayload)
-		response = LES_CreateNetworkMessage(LES_NETMESSAGE_SEND_ID_TEST, msgId, payload)
+		response = LES_CreateNetworkMessage(LES_NETMESSAGE_SEND_ID_TEST_RESPONSE, msgId, payload)
 		self.request.send(response)
 
 	def LES_HandleConnectMessage(self, msgType, msgId, msgPayloadSize, msgPayload):
-		print("Connect: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
-		print("Connect: payload:%s" % (msgPayload))
+		les_logger.Log("Connect: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
+		les_logger.Log("Connect: payload:%s" % (msgPayload))
 		hashValue = les_hash.GenerateHashCaseSensitive(msgPayload)
-		print("Connect: connectResponse:0x%X" % (hashValue))
+		les_logger.Log("Connect: connectResponse:0x%X" % (hashValue))
 		payload = packedUint32.pack(hashValue)
 		response = LES_CreateNetworkMessage(LES_NETMESSAGE_SEND_ID_CONNECT_RESPONSE, msgId, payload)
+		self.request.send(response)
+
+	def LES_HandleGetDefinitionFileMessage(self, msgType, msgId, msgPayloadSize, msgPayload):
+		les_logger.Log("GetDefinitionFile: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
+		les_logger.Log("GetDefinitionFile: payload:%s" % (msgPayload))
+		definitionFile = les_definitionfile.LES_DefinitionFile()
+		definitionFile.create()
+		chunkFileData = definitionFile.makeData()
+		definitionFileData = chunkFileData.getData()
+		payload = definitionFileData
+		response = LES_CreateNetworkMessage(LES_NETMESSAGE_SEND_ID_GETDEFINITIONFILE_RESPONSE, msgId, payload)
+		chunkFileData.close()
 		self.request.send(response)
 
 	def handle(self):
 		s_receivedMessageHandlers = {}
 		s_receivedMessageHandlers[LES_NETMESSAGE_RECV_ID_TEST] = self.LES_HandleTestMessage
 		s_receivedMessageHandlers[LES_NETMESSAGE_RECV_ID_CONNECT] = self.LES_HandleConnectMessage
+		s_receivedMessageHandlers[LES_NETMESSAGE_RECV_ID_GETDEFINITIONFILE] = self.LES_HandleGetDefinitionFileMessage
 
 		self.m_curThread = threading.currentThread()
 		self.m_threadName = self.m_curThread.getName()
@@ -87,7 +106,7 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
 			receivedMessage = LES_NetworkMessage()
 			if receivedMessage.Decode(receivedData) == False:
-				print("%s: Failed to decode message receivedDataLen:%d" % (self.m_threadName, receivedDataLen))
+				les_logger.Warning("%s: Failed to decode message receivedDataLen:%d" % (self.m_threadName, receivedDataLen))
 				continue
 
 			msgType = receivedMessage.m_type
@@ -97,17 +116,15 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 			if msgType in s_receivedMessageHandlers:
 				s_receivedMessageHandlers[msgType](msgType, msgId, msgPayloadSize, msgPayload)
 			else:
-				print("Unhandled: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
-				print("Unhandled: payload:%s" % (msgPayload))
-
-#			payload = "%s:%s" % (self.m_threadName, msgPayload)
-#			response = LES_CreateNetworkMessage(LES_NETMESSAGE_SEND_ID_TEST, msgId, payload)
-#			self.request.send(response)
+				les_logger.Warning("Unhandled: type:0x%X id:%d payloadSize:%d" % (msgType, msgId, msgPayloadSize))
+				les_logger.Warning("Unhandled: payload:%s" % (msgPayload))
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 	pass
 
 def runTest():
+	les_logger.Init()
+
 	# Port 0 means to select an arbitrary unused port
  	HOST, PORT = "localhost", 3141
 
@@ -118,7 +135,7 @@ def runTest():
  	server_thread = threading.Thread(target=server.serve_forever)
  	server_thread.setDaemon(True)
  	server_thread.start()
- 	print "Server loop running in thread:", server_thread.getName()
+ 	les_logger.Log("Server loop running in thread:%s", server_thread.getName())
 
 	while server_thread.isAlive():
 		time.sleep(1.0)
